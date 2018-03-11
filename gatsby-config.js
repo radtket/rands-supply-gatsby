225be1731a19eeeac7adb68e8b13fedb053ffbc9
Taylor Radtke <tradtke@aapa.org>
2018-03-11 17:57:07 -0400

    added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
 	siteMetadata: {
 		title: 'Gatsby Default Starter',
 	},
-	plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-sass`],
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-styled-components`,
+		{
+			resolve: 'gatsby-plugin-google-fonts',
+			options: {
+				fonts: ['roboto: 300,400,500,700,900'],
+			},
+		},
+	],
 };
