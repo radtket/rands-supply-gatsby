c9656b39a39ef281eab03e8a09a8bffe70d92249
Taylor Radtke <taylorradtke@gmail.com>
2024-12-15 13:28:53 -0600

    gh-pages

--- src/config/SiteConfig.js
+++ src/config/SiteConfig.js
@@ -1,45 +1,44 @@
 module.exports = {
-	pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  pathPrefix: "/rands-supply-gatsby",
+  siteTitle: "R&S Supply", // Navigation and Site Title
+  siteTitleAlt: "R&S Supply - Your Full Service Roofing and Building", // Alternative Site title for SEO
+  siteUrl: "https://www.randssupply.net", // Domain of your site. No trailing slash!
+  siteLanguage: "en", // Language Tag on <html> element
+  siteLogo: "/logos/logo-1024.png", // Used for SEO and manifest
+  siteDescription: "Offical website of R&S Supply Roofing Company",
 
-	siteTitle: 'R&S Supply', // Navigation and Site Title
-	siteTitleAlt: 'R&S Supply - Your Full Service Roofing and Building', // Alternative Site title for SEO
-	siteUrl: 'https://www.randssupply.net', // Domain of your site. No trailing slash!
-	siteLanguage: 'en', // Language Tag on <html> element
-	siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-	siteDescription: 'Offical website of R&S Supply Roofing Company',
+  siteFBAppID: "123456789", // Facebook App ID
+  userTwitter: "emilia", // Twitter Username
+  ogSiteName: "RandSFresno", // Facebook Site Name
+  googleAnalyticsID: "UA-12345689-1",
 
-	siteFBAppID: '123456789', // Facebook App ID
-	userTwitter: 'emilia', // Twitter Username
-	ogSiteName: 'RandSFresno', // Facebook Site Name
-	googleAnalyticsID: 'UA-12345689-1',
+  // Date format used in your project
+  dateFormat: "DD.MM.YYYY",
 
-	// Date format used in your project
-	dateFormat: 'DD.MM.YYYY',
+  // Manifest and Progress color
+  themeColor: "#001e3c",
+  backgroundColor: "#f8f8f8",
 
-	// Manifest and Progress color
-	themeColor: '#001e3c',
-	backgroundColor: '#f8f8f8',
+  // Settings for typography.js
+  headerFontFamily: "Roboto",
+  bodyFontFamily: "Roboto",
+  baseFontSize: "16px",
 
-	// Settings for typography.js
-	headerFontFamily: 'Roboto',
-	bodyFontFamily: 'Roboto',
-	baseFontSize: '16px',
-
-	// Your information
-	avatar: '/logos/social.png',
-	name: 'R&S Supply',
-	location: 'United States',
-	email: 'info@randssupply.com',
-	phone: '1555555555',
-	phoneFormated: '(555) 555-5555',
-	socialMedia: [
-		{
-			url: 'https://www.facebook.com/RandSFresno',
-			name: 'Facebook',
-		},
-		{
-			url: 'https://www.linkedin.com/company/r&s-supply',
-			name: 'LinkedIn',
-		},
-	],
+  // Your information
+  avatar: "/logos/social.png",
+  name: "R&S Supply",
+  location: "United States",
+  email: "info@randssupply.com",
+  phone: "1555555555",
+  phoneFormated: "(555) 555-5555",
+  socialMedia: [
+    {
+      url: "https://www.facebook.com/RandSFresno",
+      name: "Facebook",
+    },
+    {
+      url: "https://www.linkedin.com/company/r&s-supply",
+      name: "LinkedIn",
+    },
+  ],
 };
